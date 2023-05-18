232f9110fd42ad0fbb0b7c5e02ad00a3de20a3a9
Eievui <14899090+eievui5@users.noreply.github.com>
2023-05-18 14:47:02 -0400

    Remove some unused code

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,10 @@ use lalrpop_util::lalrpop_mod;
 use lalrpop_util::ParseError;
 use std::collections::VecDeque;
 use std::io::Read;
-use std::fmt;
 
 pub mod rules;
 lalrpop_mod!(pub rule_parser);
 pub use rule_parser::RulesParser;
-
-#[derive(Debug)]
-pub struct FaeError {
-	reason: String,
-}
-
-impl From<&str> for FaeError {
-	fn from(reason: &str) -> Self {
-		Self {
-			reason: String::from(reason)
-		}
-	}
-}
-
-impl fmt::Display for FaeError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.reason)
-    }
-}
-
-impl std::error::Error for FaeError {}
 
 type StaticParseError<'a> = ParseError<usize, lalrpop_util::lexer::Token<'a>, &'static str>;
 
@@ -44,7 +22,7 @@ fn strip_token_edges(token: &str) -> Result<String, StaticParseError> {
 	bytes.pop_back();
 	let mut result = String::new();
 	bytes.read_to_string(&mut result)
-		.map_err(|err| user_error("Invalid UTF-8"))?;
+		.map_err(|_| user_error("Invalid UTF-8"))?;
 	Ok(result)
 }
 
@@ -53,6 +31,6 @@ fn strip_token_end(token: &str) -> Result<String, StaticParseError> {
 	bytes.pop_back();
 	let mut result = String::new();
 	bytes.read_to_string(&mut result)
-		.map_err(|err| user_error("Invalid UTF-8"))?;
+		.map_err(|_| user_error("Invalid UTF-8"))?;
 	Ok(result)
 }
